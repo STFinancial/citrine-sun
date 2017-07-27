2d26441c4670a202a95b8d66386c0d7c794819fd
stfinancial <palla021@umn.edu>
2017-07-27 13:08:06 -0700

    Adding support for canceling orders of a certain type, above or below a certain price.

--- src/utils/CancelOrders.java
+++ src/utils/CancelOrders.java
@@ -8,6 +8,7 @@ import api.request.MarketResponse;
 import api.request.OpenOrderRequest;
 import api.request.OpenOrderResponse;
 import api.tmp_trade.TradeOrder;
+import api.tmp_trade.TradeType;
 import keys.KeyManager;
 
 import java.util.Collections;
@@ -20,7 +21,13 @@ import static api.Currency.*;
  * Cancels all orders for a given {@link CurrencyPair} on {@link Poloniex}.
  */
 class CancelOrders {
-    private static final CurrencyPair PAIR = CurrencyPair.of(ETH, BTC);
+    private static final CurrencyPair PAIR = CurrencyPair.of(XMR, BTC);
+    private static final boolean RESTRICT_TO_TYPE = true;
+    private static final TradeType TYPE = TradeType.BUY;
+    private static final boolean ENABLE_CANCEL_ABOVE = false;
+    private static final double CANCEL_ABOVE_PRICE = 0.000101;
+    private static final boolean ENABLE_CANCEL_BELOW = false;
+    private static final double CANCEL_BELOW_PRICE = 0.000104;
 
     public static void main(String[] args) {
         CancelOrders o = new CancelOrders();
@@ -39,12 +46,13 @@ class CancelOrders {
         Map<CurrencyPair, List<TradeOrder>> orders = ((OpenOrderResponse) r).getOpenOrders();
 
         orders.getOrDefault(PAIR, Collections.emptyList()).forEach((order) -> {
+            if (RESTRICT_TO_TYPE && order.getTrade().getType() != TYPE) return;
+            if (ENABLE_CANCEL_ABOVE && order.getTrade().getRate() <= CANCEL_ABOVE_PRICE) return;
+            if (ENABLE_CANCEL_BELOW && order.getTrade().getRate() >= CANCEL_BELOW_PRICE) return;
             System.out.println(p.processMarketRequest(new CancelRequest(order.getOrderId(), CancelRequest.CancelType.TRADE)).getJsonResponse());
             try {
                 Thread.sleep(250);
-            } catch (InterruptedException e) {
-
-            }
+            } catch (InterruptedException e) {}
         });
     }
 }
